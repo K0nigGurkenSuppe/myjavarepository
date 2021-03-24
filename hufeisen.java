268ec769620e9b2c9c982d7a346aeda0daae29c3
K0nigGurkenSuppe <54551493+K0nigGurkenSuppe@users.noreply.github.com>
2021-03-24 10:13:13 +0100

    Maybee fix?
    Ka kann funktionieren

--- hufeisen.java
+++ hufeisen.java
@@ -3,15 +3,16 @@ import javax.swing.*;
 public class hufeisen {
 
 	public static void main(String[] args) {
-		JOptionPane.showMessageDialog(null, "Bezahlvorgang", "Hufeisenschmied Checkout", JOptionPane.PLAIN_MESSAGE);
 
 		int price = 0;
 		int iC = 1;
+		int priceG = 0;
 		for(iC = 1; iC <= 24; iC++) {
 
-			price = iC*iC -1;
+			price = iC*iC;
+			priceG = iC + priceG;
             if(price == 0) { price = 1; };if(price == 8) { price = 7; };
-			System.out.println("Der " + iC + " Nagel kostet " + price);
+			System.out.println("Der " + iC + " Nagel kostet " + priceG + " Pfenning. Gesammtpreis ist " + price);
 
 		}
 		JOptionPane.showMessageDialog(null,"Auswertung:\nDer Gesammtpreis liegt bei " + price + " Pfenning\n1 Silbermark ist umgerechnet 256 Pfenning.\nEs wurden somit umgerechnet " + price / 256 + " Mark Gezahlt!", "Hufeisenschmied Checkout", JOptionPane.PLAIN_MESSAGE);
